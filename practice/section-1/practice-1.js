9a438cceff7c19df51a414f3ff3b206edb1a8c23
Viggges <623656827@qq.com>
2019-08-19 18:44:21 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var x=new Array();
+  for(var i=0;i<collectionA.length;i++){
+    if(collectionB.indexOf(collectionA[i])!== -1){
+      x.push(collectionA[i]);
+    }
+  }
+  var m=['a','e','f','d'];
+  return x;
 }
